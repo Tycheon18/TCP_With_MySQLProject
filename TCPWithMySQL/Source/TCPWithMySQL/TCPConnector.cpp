1868574ae2c37b03eec1757befb71d22f7483cb5
Tycheon18 <joong0818@gmail.com>
2024-02-17 23:54:25 +0900

    First Commit

---
+++ TCPWithMySQL/Source/TCPWithMySQL/TCPConnector.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TCPConnector.h"
+
+// Sets default values
+ATCPConnector::ATCPConnector()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATCPConnector::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATCPConnector::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
